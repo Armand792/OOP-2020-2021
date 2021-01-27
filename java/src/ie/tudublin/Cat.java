bf4342268542a0a405150e4316a407d36e926028
Armando Cardozo <armandopatricio704@hotmail.com>
2021-01-27 19:49:20 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -1,9 +1,32 @@
 package ie.tudublin;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class Cat extends Animal
 {
     public Cat(String name)
     {
         super(name);
     }
+
+    private int numLives;
+
+
+    public void setLives(int numLives)
+    {
+        this.numLives = numLives;
+    }
+
+    public void kill() {
+
+        numLives = numLives - 1;
+    
+        if (numLives>0){
+            System.out.println("OUCH!");
+        }
+        else{
+            System.out.println("Dead");
+        }
+    }
+
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -4,27 +4,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+ 
+        Cat ginger = new Cat ("Ginger");
 
-        Animal misty = new Dog("Misty");
+        
+        ginger.setLives(9);
+        ginger.kill();
 
-        Animal topCat = new Cat("TopCat");
-
-        System.out.println(misty);
-        System.out.println(topCat);
-
-        misty = topCat;
-
-        topCat.setName("Garfield");
-
-        System.out.println(misty);
-        System.out.println(topCat);
-
-        // What will get printed out??
-
-        // 1. topcat, Garfield
-        // 2. garfield, Garfield 
-
-
+        for (int i = 0; i < 8; i++) {
+            ginger.kill();
+          }
+          
     }
+
 } 
